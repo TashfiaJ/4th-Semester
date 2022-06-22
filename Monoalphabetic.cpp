7c63bfcec473153fb42b478a32b58858cafc69cd
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-06-22 12:16:03 +0600

    Update and rename Monoalphabetic.c to Monoalphabetic.cpp

--- Monoalphabetic.cpp
+++ Monoalphabetic.cpp
@@ -8,7 +8,7 @@ string p="",c="",r="";
 void encryption(){
         for(ll i=0;i<p.size();i++)
             {
-                for(j=0;j<26;j++)
+                for(ll j=0;j<26;j++)
                     {
                         if(pt[j]==p[i])
                         {
@@ -18,27 +18,32 @@ void encryption(){
             }
             cout << "cipher text is: " << c << '\n';
 }
+
+void decryption()
+{
+            
+            for(ll i=0;i<c.size();i++)
+            {
+                        for(ll j=0;j<26;j++)
+                        {
+                        if(ct[j]==c[i])
+                        {
+                                    r+=pt[j];
+                        }
+                        }
+            }
+            cout << "plain text is: " << r;
+}
+
 int main()
 {
             
             
-            int i,j;
             
-            cout << "\n enter the plain text: ";
+            
+            cout << "\nenter the plain text: ";
             cin >> p;
-            
-
-            //converting cipher text into plain text (decryption)
-            for(i=0;i<strlen(c);i++)
-            {
-                        for(j=0;j<26;j++)
-                        {
-                        if(ct[j]==c[i])
-                        {
-                                    r[i]=pt[j];
-                        }
-                        }
-            }
-            printf("\n \n plain text is: %s",r);
+            encryption();
+            decryption(); 
            
 }
