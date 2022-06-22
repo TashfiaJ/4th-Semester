4374812b9e594bf01f3353c6a30269e433714fc9
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-06-22 11:29:19 +0600

    Update Monoalphabetic.c

--- Monoalphabetic.c
+++ Monoalphabetic.c
@@ -1,27 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+char pt[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char ct[26]={'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'};
+string p="",c="",r="";
 
-int main()
-{
-            char pt[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-            char ct[26]={'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'};
-            char p[20]={'\0'},c[20]={'\0'},r[20]={'\0'};
-            int i,j;
-            
-            printf("\n enter the plain text:");
-            cin >> p;
-            //converting plain text into cipher text (encryption)
-            for(i=0;i<strlen(p);i++)
+void encryption(){
+        for(ll i=0;i<p.size();i++)
             {
-                        for(j=0;j<26;j++)
-                        {
+                for(j=0;j<26;j++)
+                    {
                         if(pt[j]==p[i])
                         {
-                                    c[i]=ct[j];
+                                    c+=ct[j];
                         }
                         }
             }
-            printf("\n cipher text is: %s",c);
+            cout << "cipher text is: " << c << '\n';
+}
+int main()
+{
+            
+            
+            int i,j;
+            
+            cout << "\n enter the plain text: ";
+            cin >> p;
+            
 
             //converting cipher text into plain text (decryption)
             for(i=0;i<strlen(c);i++)
